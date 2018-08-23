85b9470426de945e07072001d601391e320fb9fb
madadi <creawebdesigner@gmail.com>
2018-08-23 16:09:27 +0200

    chromaticity1

---
+++ script.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.3.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA5T89ltH71mJ3aLWEUb-5QugHiQaBmLcE",
+    authDomain: "cd-monprojet.firebaseapp.com",
+    databaseURL: "https://cd-monprojet.firebaseio.com",
+    projectId: "cd-monprojet",
+    storageBucket: "cd-monprojet.appspot.com",
+    messagingSenderId: "69335341797"
+  };
+  firebase.initializeApp(config);
+</script>
